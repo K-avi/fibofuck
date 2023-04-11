8fcbbccb3bb46e6f9fd5d4b3ed59726c8ba09ee7
ivan <i.m-r.ecole@orange.fr>
2023-04-11 16:01:41 +0200

    gotta take my time w the skew heap implem to make sure it works fine

--- src/fibo.c
+++ src/fibo.c
@@ -11,11 +11,8 @@
 //pain ; suffering even 
 
 /*
-
     -> function to generate the skew heaps; 
-
     -> function to put em in the doubly linked list (this part is ez )
-
     -> proper manipulation of fib heap (here comes the pain)
 */
 
@@ -28,7 +25,10 @@ S_NODE * initNode(int key){
     initialises a tree node and returns it
     */
     S_NODE* ret = (S_NODE*) malloc(sizeof(S_NODE));
-
+    if(!ret){
+        fprintf(stderr,"error : allocation of heap failed\n");
+        return NULL;
+    }
     ret->key= key;
     ret->lchild=ret->rchild=ret->parent=NULL;
 
@@ -48,6 +48,7 @@ void freeSkewHeap(S_NODE * heap){
     }
 }//not tested
 
+
 /* skew heap manipulation function */
 
 void swapKey( S_NODE * node1, S_NODE* node2){
@@ -58,18 +59,25 @@ void swapKey( S_NODE * node1, S_NODE* node2){
         node1->key= node2->key;
         node2->key= tmp;
     }
-}//not tested; prolly ok though lol
+}//not tested; prolly ok though 
+//maybe useless
 
-void swapNodes ( S_NODE ** node1 , S_NODE ** node2){
+void swapNodes ( S_NODE * node1 , S_NODE * node2){
         /*
         swaps the node ptr so be careful with this
         */
-        if(! (node1 && node2)) return;
-        S_NODE * tmp = *node1;
 
-        *node1= *node2;
-        *node2= tmp;
-}
+        if(! (node1 && node2)) return;
+        S_NODE * tmp = node1;
+        S_NODE *tmpParent1 = node1->parent, *tmpParent2= node2->parent;
+        
+        node1= node2;
+        node2= tmp;
+
+        node2->parent= tmpParent1;
+        node1->parent= tmpParent2;
+}//not tested 
+//prolly an issue w parents
 
 S_NODE * mergeHeaps( S_NODE * SkewHeap1, S_NODE * SkewHeap2){
     /*
@@ -85,14 +93,40 @@ S_NODE * mergeHeaps( S_NODE * SkewHeap1, S_NODE * SkewHeap2){
         return SkewHeap1;
     }else{ //both are non null
 
-        if(SkewHeap1->key > SkewHeap2->key) swapNodes(&SkewHeap1, &SkewHeap2);
+        if(SkewHeap1->key > SkewHeap2->key){
+             swapNodes(SkewHeap1, SkewHeap2);
+        }
 
-        return NULL;
+        S_NODE * tmp = SkewHeap1->lchild;
+      
+
+        SkewHeap1->lchild = SkewHeap1->rchild; 
+        SkewHeap1->rchild= tmp; 
+
+        SkewHeap1->lchild= mergeHeaps(SkewHeap2 , SkewHeap1->lchild);
+
+        return SkewHeap1;
     }
 
 
 
-}//not done lol
+}//not tested 
+//prolly an issue with the parent though
+
+void insertHeap(S_NODE* skHeap, int value){
+    /*
+    doesn't check for null bc skHeap can be empty and the new node becomes the only node in the heap
+    */
+
+    S_NODE* newNode= initNode(value); 
+    if(!newNode){
+        fprintf(stderr, "failed to allocate memory for a new node in the heap\n");
+        return;
+    }
+    skHeap= mergeHeaps(skHeap, newNode);
+
+
+}//not tested
 
 void decrNode( S_NODE * heapNode ){
     /*
@@ -117,21 +151,55 @@ void decrNode( S_NODE * heapNode ){
     }
 }//not tested 
 
-void popRoot (S_NODE * skewHeap){
+int popRoot (S_NODE * skHeap){
+    /*
+    returns 0 if nullptr passed
+    */
+   if(!skHeap) return 0;
 
-}
+   S_NODE * skL =  skHeap->lchild, *skR= skHeap ->rchild;
 
-void popNode (S_NODE * heapNode){
+    int ret = skHeap->key; 
+    S_NODE * tmp = skHeap; 
+
+    skHeap = mergeHeaps(skL, skR);
+
+    free(tmp);
+
+    return ret; 
+}//not tested; 
+
+int popNode (S_NODE * heapNode){
+    /* 
+    frees node passed; merges it's children ; 
+    and then merges the result with it's parent
+
+    returns 0 if nullptr passed
+    */
+    if(! heapNode) return 0; 
+
+    S_NODE * parent= heapNode->parent;
+
+    S_NODE * newBranch= mergeHeaps(heapNode->lchild, heapNode->rchild);
+    if(newBranch) newBranch->parent= NULL;
     
-}
+    int ret = heapNode->key;
+
+    free(heapNode); 
+    heapNode= newBranch;
+
+    heapNode->parent= parent; 
+    return ret; 
+
+}//incorrect; do not use ; not good at all 
 
 void minHeapify ( S_NODE *  heapNode){
 
-}
+}//not done
 
 void incrNode( S_NODE * heapNode){
 
-}
+}//not done
 
 /* DOUBLY LINKED LIST  */
 
